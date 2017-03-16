2ba9c37c4fbfb64a35fec4d8033770d90dde8491
pkemalov <petar.kemalov@gmail.com>
2017-03-17 00:17:48 +0200

    commited

---
+++ 02.defaultValues/DefaultValues.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.defaultValues
+{
+    public class DefaultValues
+    {
+        public static void Main()
+        {
+            var line = Console.ReadLine();
+
+            var defaultValueDict = new Dictionary<string, string>();
+            
+
+            while (line != "end")
+            {
+                var input = line
+                    .Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                var inputName = input.First();
+                var inputValue = input.Last();
+
+                defaultValueDict[inputName] = inputValue;
+                                
+                line = Console.ReadLine();
+            }
+            string lineSecond = Console.ReadLine();
+
+            var sortedDict = defaultValueDict
+                .Where(x => x.Value != "null")
+                .OrderByDescending(x => x.Value.Length)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            var unsortedDict = defaultValueDict
+                .Where(x => x.Value == "null")
+                .ToDictionary(x => x.Key, x => lineSecond);
+
+            
+            foreach (var itemPair in sortedDict)
+            {
+                var name = itemPair.Key;
+                var value = itemPair.Value;
+
+                Console.WriteLine($"{name} <-> {value}");
+            }
+
+            foreach (var itemPair in unsortedDict)
+            {
+                var name = itemPair.Key;
+                var value = itemPair.Value;
+
+                Console.WriteLine($"{name} <-> {value}");
+            }
+
+        }
+    }
+}
---
+++ LambdaLinqExercises/RegisteredUsers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace LambdaLinqExercises
+{
+    public class RegisteredUsers
+    {
+        public static void Main()
+        {
+            string inputEntered = Console.ReadLine();
+            var registeredUsers = new Dictionary<String, DateTime>();
+
+            while (inputEntered!="end")
+            {
+                var entry = inputEntered
+                    .Split(' ')
+                    .ToList();
+
+                var firstElement = entry.First();
+                DateTime lastElement = DateTime.ParseExact(entry.Last(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                //най-стандартен начин за вкарване в речника
+
+                registeredUsers[firstElement] = lastElement;
+
+                //по-засукания начин за вкарване в речника
+                                
+                //if (!registeredUsers.ContainsKey(firstElement))
+                //{
+                //    registeredUsers[firstElement] = lastElement;
+                //}
+                inputEntered = Console.ReadLine();
+            }
+            var finalDictionary = registeredUsers
+                .Reverse()
+                .OrderBy(x => x.Value)
+                .Take(5)
+                .OrderByDescending(x => x.Value)
+                .ToDictionary(x => x.Key, y => y.Value);
+
+            foreach (var itemPair in finalDictionary)
+            {
+                var name = itemPair.Key;
+                
+                Console.WriteLine($"{name}");
+            }
+
+        }
+    }
+}
